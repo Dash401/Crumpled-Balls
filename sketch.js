19d4b23b125b4250604fee69c31295aa0110217c
Dash401 <67057642+Dash401@users.noreply.github.com>
2021-01-18 23:47:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var engine,gameState, world;
+
+var ball;
+var ground;
+var bin1,bin2,bin3;
+
+
+
+
+function setup() {
+	createCanvas(800, 700);
+
+	engine = Engine.create();
+	world = engine.world;
+	gameState = "start";
+	ball = new Ball(300,200,10);
+
+	ground = Bodies.rectangle(width/2,600,800,20,{isStatic: true});
+	World.add(world,ground);
+
+	bin1 = new Dustbin(700,580,100,20);
+	bin2 = new Dustbin(640,540,20,100);
+	bin3 = new Dustbin(760,540,20,100);
+
+	Engine.run(engine);
+	//console.log(ball);
+}
+
+
+function keyPressed(){
+	if (keyCode === UP_ARROW) {
+	  Matter.Body.applyForce(ball.body, ball.body.position, {
+		x: 12,
+		y: -13
+	  });
+	}
+}
+
+function draw() {
+	rectMode(CENTER);
+	background(0);
+	
+	ball.display();
+	
+
+	bin1.display();
+	bin2.display();
+	bin3.display();
+	Engine.update(engine);
+	
+
+} 
